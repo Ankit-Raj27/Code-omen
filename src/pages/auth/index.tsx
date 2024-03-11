db9496dea4484b06be6ed52d203a0f4346cd5058
Ankit-Raj27 <ankitrajk27@gmail.com>
2024-03-11 20:15:46 +0530

    [Added a basic Navbar, sign in button and logo]

---
+++ src/pages/auth/index.tsx
@@ -0,0 +1,20 @@
+import Navbar from '@/components/Navbar';
+import Image from 'next/image';
+import React from 'react';
+
+type AuthPageProps = {
+    
+};
+
+const AuthPage:React.FC<AuthPageProps> = () => {
+    
+    return <div className='bg-gradient-to-b from-gray-600 to-black h-screen relative'>
+            <div className='max-w-7xl mx-auto'>
+                <Navbar />
+                <div className="flex items-center justify-center h-[calc(100vh-5rem)] pointer-events-none select-none">
+                    <Image src="/hero.png" width={600} height={100} alt="hero"/>
+                </div>
+            </div>
+        </div>
+}
+export default AuthPage;
