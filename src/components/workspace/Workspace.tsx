45adf9cafabbb50420b76e1e269a1c06c9012f5e
Ankit-Raj27 <ankitrajk27@gmail.com>
2024-04-02 16:41:16 +0530

    [Added problems]

--- src/components/workspace/Workspace.tsx
+++ src/components/workspace/Workspace.tsx
@@ -2,12 +2,15 @@ import React from "react";
 import Split from "react-split";
 import ProblemDescription from "./problemDesc/ProblemDescription";
 import Playground from "./playground/Playground";
-type WorkspaceProps = {};
+import { Problem } from "@/utils/types/problems";
+type WorkspaceProps = {
+  problem: Problem
+};
 
-const Workspace: React.FC<WorkspaceProps> = () => {
+const Workspace: React.FC<WorkspaceProps> = ({problem}) => {
   return (
     <Split className="split" minSize={0}>
-      <ProblemDescription />
+      <ProblemDescription problem={problem}/>
       <Playground />
     </Split>
   );
--- src/pages/problems/[pid].tsx
+++ src/pages/problems/[pid].tsx
@@ -1,15 +1,49 @@
-import React from 'react';
-import TopBar from '../../components/topBar/TopBar';
-import Workspace from '@/components/workspace/Workspace';
+import React from "react";
+import TopBar from "../../components/topBar/TopBar";
+import Workspace from "@/components/workspace/Workspace";
+import { problems } from "@/utils/problems";
+import { Problem } from "@/utils/types/problems";
 type ProblemPageProps = {
-    
+  problem: Problem;
 };
 
-const ProblemPage:React.FC<ProblemPageProps> = () => {
-    
-    return <div>
-        <TopBar problemPage/>
-        <Workspace />
+const ProblemPage: React.FC<ProblemPageProps> = ({ problem }) => {
+  console.log(problem);
+  return (
+    <div>
+      <TopBar problemPage />
+      <Workspace problem={problem} />
     </div>
+  );
+};
+export default ProblemPage;
+
+//fetch the local data
+//Static site generation
+// getStaticPath =? creates dynamic routes
+
+export async function getStaticPaths() {
+  const paths = Object.keys(problems).map((key) => ({
+    params: { pid: key },
+  }));
+  return {
+    paths,
+    fallback: false,
+  };
 }
-export default ProblemPage; 
+
+export async function getStaticProps({ params }: { params: { pid: string } }) {
+  const { pid } = params;
+  const problem = problems[pid];
+  if (!problems) {
+    return {
+      notFound: true,
+    };
+  }
+  problem.handlerFunction = problem.handlerFunction.toString();
+  return {
+    props: {
+      problem,
+    },
+  };
+}
