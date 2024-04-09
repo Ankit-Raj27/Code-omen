79f10695a4732a9cbc52671ee250903d7981cfbd
Ankit-Raj27 <ankitrajk27@gmail.com>
2024-04-09 14:31:18 +0530

    [changed signup card]

--- src/components/Modals/Signup.tsx
+++ src/components/Modals/Signup.tsx
@@ -75,7 +75,7 @@ const Signup: React.FC<SignupProps> = () => {
 
   return (
     <form className="space-y-6 px-6 py-4" onSubmit={handleRegister}>
-      <h3 className="text-xl font-medium text-black"> Register now!</h3>
+      <h3 className="text-xl font-medium text-white"> Register now!</h3>
       <div>
         <label
           htmlFor="email"
@@ -129,7 +129,7 @@ const Signup: React.FC<SignupProps> = () => {
       </div>
       <button
         type="submit"
-        className="w-full text-white focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center 
+        className="w-full text-black focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center 
       bg-brand-orange hover:bg-brand-orange-s"
       >
         {loading ? "Registering..." : "Register"}
