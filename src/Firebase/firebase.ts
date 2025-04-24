ad1ebbb4c9b70db71a9859e845e38598944ee639
Ankit-Raj27 <ankitrajk27@gmail.com>
2025-04-24 14:31:34 +0530

    [fixed firebase]

--- src/Firebase/firebase.ts
+++ src/Firebase/firebase.ts
@@ -19,7 +19,8 @@ const firebaseConfig = {
 };
 
 
-const app = !getApps.length ? initializeApp(firebaseConfig) : getApp(); 
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+
 const auth = getAuth(app);
 
 const firestore = getFirestore(app);
